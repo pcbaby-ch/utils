26fadbe7d75fcb8e327b8dcc007cdb32793d872e
ack <ack@DESKTOP-UCCH0VD.lan>
2019-11-23 16:39:10 +0800

    restTemplate

--- spring-boot-web/src/main/java/com/ack/common/InvokeService.java
+++ spring-boot-web/src/main/java/com/ack/common/InvokeService.java
@@ -1,17 +1,121 @@
 package com.ack.common;
 
-import com.alibaba.fastjson.JSONObject;
-import com.ack.enums.RespCode;
 
-import org.apache.commons.lang.StringUtils;
+import java.util.concurrent.Future;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.client.RestTemplate;
 
+import com.ack.enums.RespCode;
+import com.alibaba.fastjson.JSONObject;
+
+/**
+ * 多线程异步restful服务调用
+ * 
+ * @author chen.zhao @DATE: Apr 17, 2018
+ */
 @Component
 public class InvokeService {
 
 	private static Logger log = LoggerFactory.getLogger(InvokeService.class);
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	/**
+	 * 异步调用
+	 * 
+	 * @param url
+	 * @param req
+	 * @return
+	 */
+	@Async("getThreadPoolTaskExecutor")
+	public Future<String> asynInvoke(String url, Object req) {
+		long startTime = System.currentTimeMillis();
+		log.info(">>>START asynInvoke:[{}],,#reqData:[{}],,[{}]", url, JSONObject.toJSON(req), Thread.currentThread());
+		Future<String> future = new AsyncResult<>(restTemplate.postForObject(url, req, String.class));
+		log.info(">>>END asynInvoke:[{}],,#invoke interval time[{}]	", url, System.currentTimeMillis() - startTime);
+		return future;
+	}
+
+	public String synInvoke(String url, Object req, int timeout) {
+		HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+		factory.setConnectTimeout(3000);
+		factory.setReadTimeout(timeout);
+		RestTemplate restTemplate2 = new RestTemplate(factory);
+		long startTime = System.currentTimeMillis();
+		log.info(">>>START synInvoke:[{}],,#reqData:[{}]", url, JSONObject.toJSON(req));
+		String resp;
+		try {
+			resp = restTemplate2.postForObject(url, req, String.class);
+		} catch (Exception e) {
+			throw new ServiceException(RespCode.RESTFUL_REQ_TIMEOUT);
+		}
+		log.info(">>>END synInvoke:[{}],,#invoke interval time[{}]", url, System.currentTimeMillis() - startTime);
+		return resp;
+	}
+
+	/**
+	 * 同步调用
+	 * 
+	 * @param url
+	 * @param req
+	 * @return
+	 */
+	public String synInvoke(String url, Object req) {
+		long startTime = System.currentTimeMillis();
+		log.info(">>>START synInvoke:[{}],,#reqData:[{}]", url, JSONObject.toJSON(req));
+		String resp;
+		try {
+			resp = restTemplate.postForObject(url, req, String.class);
+		} catch (Exception e) {
+			throw new ServiceException(RespCode.RESTFUL_REQ_TIMEOUT);
+		}
+		log.info(">>>END synInvoke:[{}],,#invoke interval time[{}],,#resp:[{}]", url,
+				System.currentTimeMillis() - startTime, resp);
+		return resp;
+	}
+
+	/**
+	 * 解析响应数据，直接返回整个报文体jsonObj
+	 * 
+	 * @param fuResp
+	 * @return
+	 */
+	public JSONObject parseResp(Future<String> fuResp) {
+		String result = null;
+		try {
+			result = fuResp.get();
+		} catch (Exception e) {
+			throw new ServiceException(RespCode.RESTFUL_REQ_TIMEOUT);
+		}
+		// #解析服务响应数据>JSONObject
+		return parse(result);
+	}
+
+	/**
+	 * 重解析响应数据，响应码非0000报错，否则返回data部分
+	 * 
+	 * @param fuResp
+	 * @return
+	 */
+	public JSONObject parseRespData(Future<String> fuResp) {
+		String result = null;
+		try {
+			result = fuResp.get();
+		} catch (Exception e) {
+			throw new ServiceException(RespCode.RESTFUL_REQ_TIMEOUT);
+		}
+		// #解析服务响应数据>JSONObject
+		return parseData(result);
+	}
 
 	/**
 	 * 重解析响应数据，响应码非0000报错，否则返回data部分
@@ -50,13 +154,14 @@ public class InvokeService {
 			result = result.replaceAll("\"", "'");
 			jsonObj = JSONObject.parseObject("{\"wrapResp\":\"" + result + "\"}");
 		}
-		if(jsonObj==null||jsonObj.toString().length()<=0){
+		if (jsonObj == null || jsonObj.toString().length() <= 0) {
 			throw new ServiceException(RespCode.RESTFUL_REQ_RESPERROR, jsonObj);
 		}
 		if (!(RespCode.SUCCESS.getCode().equals(jsonObj.getString("retCode"))
 				|| RespCode.SUCCESS.getCode().equals(jsonObj.getString("code")))) {
-			String code=StringUtils.isBlank(jsonObj.getString("retCode"))?jsonObj.getString("code"):jsonObj.getString("retCode");
-			throw new ServiceException(code,RespCode.RESTFUL_REQ_SERVICEERROR, jsonObj);
+			String code = StringUtils.isEmpty(jsonObj.getString("retCode")) ? jsonObj.getString("code")
+					: jsonObj.getString("retCode");
+			throw new ServiceException(code, RespCode.RESTFUL_REQ_SERVICEERROR, jsonObj);
 		}
 		return jsonObj;
 	}
@@ -75,4 +180,6 @@ public class InvokeService {
 			return jsonObj.getJSONObject("data");
 		}
 	}
+
+
 }
